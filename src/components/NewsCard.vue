8025ed47086bf7f340861d33b4c06dd2378022d6
Juan Polanco <jspolancor@gmail.com>
2019-09-28 17:30:46 -0500

    Agrega el medidor de fiabilidad

--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -7,6 +7,13 @@
         <div id="positive-votes">
           <small> Votos positivos: {{news.positive_votes.length}}</small>
         </div>
+        <h1>Veracidad {{ reliability }} %</h1>
+
+        <div id="gradient">
+            <div id="caret" :style="{
+                left: reliability + '%'
+            }"></div>
+        </div>
         <div id="related-news" class="related-news" v-if="relatedNews.length > 0" v-for="(rnews, url) in relatedNews" :key="url">
             <h4>Noticia relacionada: <a target="blank" :href=news.url >{{rnews.url}}</a></h4>
             <div >
@@ -43,7 +50,14 @@ export default {
     },
    computed:{
        ...mapState('app', ['news']), 
-   }    
+       reliability() {
+           if(this.news && this.news.negative_votes.length > 0 && this.news.positive_votes.length > 0) {
+               const total = this.news.negative_votes.length + this.news.positive_votes.length;
+               return this.news.positive_votes.length * 100 / total;
+           }
+           return 0;
+       }
+   }
     
 }
 </script>
@@ -53,6 +67,28 @@ export default {
     padding: 20px;   
     margin-top: 20px;
     border-right: 1px solid grey;
+}
+
+#news-card a {
+    word-break: break-word;
+}
+
+#gradient {
+    background: linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(9,121,25,1) 61%, rgba(33,255,0,1) 100%);
+    width: 100%;
+    height: 10px;
+    position: relative;
+}
+
+#caret {
+    width: 10px;
+    background: black;
+    height: 15px;
+    position: absolute;
+        
+    top: -8px;
+    background: white;
+    border: solid black 4px;
 }
 
 </style>
