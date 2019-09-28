fb09e1ef863e8a7bd7cb4a1d7a11e1de9e4566b1
Fabian Dc <fcaicedo@merqueo.com>
2019-09-28 12:24:09 -0500

    Componente para ver la informaion de la noticia

---
+++ src/components/NewsCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="news-card">
+        <h2>{{news.url}}</h2>
+        <div id="negative-votes">
+            {{news.negative_votes.length}}
+        </div>
+        <div id="positive-votes">
+            {{news.positve_votes.length}}
+        </div>
+        <div id="positive-comments">
+            <div>
+                {{ comment.comment}} {{comment.datetime}}
+            </div>
+        </div>
+        <div id="negative-comments">
+                {{ comment.comment}} {{comment.datetime}}
+        </div>
+    </div>
+</template>
+<script>
+import { mapMutations, mapState, mapActions } from 'vuex'
+
+export default {
+    props: {
+        news: Object
+    }
+    
+}
+</script>
+<script>
+export default {
+    
+}
+</script>
